7a4711e06b4b26acc3de97186410b58ba621b5c2
Francisco <fakico@gmail.com>
2016-01-13 19:25:43 -0500

    Ejemplo2
    Angular Ejemplo 2

---
+++ Ejemplo2/Ejemplo2/Ejemplo2/js/app.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+/* App Module */
+
+var catApp = angular.module('catApp', ['ngRoute', 'candidatocatControllers']);
+
+catApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/cand', {
+            templateUrl: 'partials/cat-list.html',
+            controller: 'CandidatoListCtrl'
+        }).
+        when('/cand/:cantId', {
+            templateUrl: 'partials/cat-detail.html',
+            controller: 'CandidatoDetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/cand'
+        });
+  }]);
---
+++ Ejemplo2/Ejemplo2/Ejemplo2/js/controller.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+
+/* Controllers */
+
+var candidatocatControllers = angular.module('candidatocatControllers', []);
+
+candidatocatControllers.controller('CandidatoListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+      $http.get("http://jsonplaceholder.typicode.com/posts").success(function (data) {
+          $scope.candidatos =[
+            {
+                "age": 63, 
+                "id": "A Candidato 1", 
+                "imageUrl": "img/descarga.png",
+                "name": "A Candidato1",
+                "contenido": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+            }, 
+            {
+                "age": 62, 
+                "id": "B Candidato 2", 
+                "imageUrl": "img/descarga.png",
+                "name": "B Candidato2",
+                "contenido": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+            },
+            {
+               "age": 61,
+               "id": "C Candidato 3",
+               "imageUrl": "img/descarga.png",
+               "name": "C Candidato3",
+               "contenido": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+            },
+            {
+               "age": 60,
+               "id": "D Candidato 2",
+               "imageUrl": "img/descarga.png",
+               "name": "D Candidato2",
+               "contenido": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+            }];
+      });
+
+      $scope.orderProp = 'age';
+  }]);
+
+candidatocatControllers.controller('CandidatoDetailCtrl', ['$scope', '$routeParams',
+  function ($scope, $routeParams) {
+      $scope.cantId = $routeParams.cantId;
+  }]);
